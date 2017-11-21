e3ad2d95cff43f3d49be0c907481dbacd9efa145
Amador368 <amadorsantiago368@gmail.com>
2017-11-20 22:03:42 -0600

    Adding styles for listView

--- src/components/dashboardView.js
+++ src/components/dashboardView.js
@@ -105,23 +105,38 @@ const styles = StyleSheet.create({
       color: 'white'
     },
     container: {
+        justifyContent: 'center',
         alignItems: 'center',
-        justifyContent: 'center'
+        backgroundColor: '#F5FCFF',
     },
     title: {
-
+        fontSize: 27,
+        marginBottom: 8,
+        textAlign: 'center',
+        color: '#FFFFFF',
+        backgroundColor: 'rgba(52,52,52,0)',
     },
     listView:{
-
+        //paddingTop: 64,
+        //marginBottom: 49,
     },
     available: {
-
+        fontSize:18,
+        textAlign: 'center',
+        color: '#FFFFFF',
+        backgroundColor:'rgba(52,52,52,0)',
     },
     backgroundImage: {
-
+        justifyContent: 'center',
+        alignItems: 'center',
+        alignSelf: 'stretch',
+        height:150,
     },
     rightContainer: {
-
+        backgroundColor:'rgba(52,52,52,0.5)',
+        alignSelf: 'stretch',
+        paddingTop:30,
+        height: 150,
     }
 
   })
